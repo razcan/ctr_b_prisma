f8fb1a3069963d878e81bf26f75bed74d8ab400e
razcan <razvan.mustata@gmail.com>
2024-05-04 09:44:35 +0300

    backend: Sat May  4 09:44:35 EEST 2024

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 require("dotenv").config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [
       'http://localhost:5500', //the ip address of the frontend server
@@ -11,6 +13,18 @@ async function bootstrap() {
     methods: ["GET", "POST", "DELETE", "PATCH"],
     credentials: true,
   });
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Contracts')
+    .setDescription('The Contracts App API description')
+    .setVersion('1.0')
+    .addTag('contracts')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
