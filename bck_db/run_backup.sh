9b3c1de3314009bf20aef3a59757a9e9268969e4
Razvan Mustata <razvan.mustata@gmail.com>
2024-09-06 16:23:46 +0300

    backend: Fri Sep  6 16:23:46 EEST 2024

--- bck_db/run_backup.sh
+++ bck_db/run_backup.sh
@@ -1,4 +1,5 @@
-cd /usr/local/Cellar/libpq/16.2_1/bin/
+
+cd /usr/local/Cellar/postgresql@16/16.4/bin
 # Original filename
 original_filename="backup.sql"
 
@@ -11,4 +12,4 @@ new_filename="${original_filename%.*}_${timestamp}.${original_filename##*.}"
 # Perform your operation using the new filename
 echo "New filename: $new_filename"
 
-PGPASSWORD=123456 ./pg_dump -U sysadmin -d contracte > /Users/razvanmustata/Projects/contracts/backend/bck_db/$new_filename
+PGPASSWORD=123456 ./pg_dump -U postgres -p 5433 -d Contracts > /Users/razvanmustata/Projects/contracts/backend/bck_db/$new_filename
--- src/multer.config.ts
+++ src/multer.config.ts
@@ -1,10 +1,28 @@
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { diskStorage } from 'multer';
+import { join } from 'path';
 
 // const folderPath = path.join(__dirname, 'upload');
 
 const storage: MulterOptions['storage'] = diskStorage({
-  destination: '/', // Specify the directory where files will be saved
+  // destination: '/Users/razvanmustata/Projects/contracts/backend/Uploads', // Specify the directory where files will be saved
+  destination: (req, file, callback) => {
+    // if (req.originalUrl.includes('/contracts/file/')) {
+    //   const uploadPath = join(__dirname, '..', 'Uploads/Contracts');
+    //   callback(null, uploadPath);
+    // } else if (req.originalUrl.includes('/nomenclatures/partnerlogo/')) {
+    //   const uploadPath = join(__dirname, '..', 'Uploads/Logos');
+    //   callback(null, uploadPath);
+    // } else if (req.originalUrl.includes('/nomenclatures/user/')) {
+    //   const uploadPath = join(__dirname, '..', 'Uploads/Avatars');
+    //   callback(null, uploadPath);
+    // } else {
+    //   const uploadPath = join(__dirname, '..', 'Uploads/Avatars');
+    //   callback(null, uploadPath);
+    // }
+    const uploadPath = join(__dirname, '..', 'Uploads'); // Creates a path relative to the current file
+    callback(null, uploadPath);
+  },
   filename(req, file, callback) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     callback(
